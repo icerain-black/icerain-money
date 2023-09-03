ae6631a348efd9f70ff79b457cb7b70b2ac5d2e8
icerain <616964638@qq.com>
2023-09-04 02:35:26 +0800

    添加nav组件点击后的class

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="icon-licai"></Icon>
       管账
     </router-link>
-    <router-link to="/labels" class="item">
-      <Icon name="icon-24gf-tags"></Icon>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="icon-biaoqian"></Icon>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="icon-tongji"></Icon>
       统计
     </router-link>
@@ -35,6 +35,10 @@ nav {
     justify-content: center;
     align-items: center;
     flex-direction: column;
+
+    &.selected{
+      color: red;
+    }
 
     .icon {
       width: 32px;
